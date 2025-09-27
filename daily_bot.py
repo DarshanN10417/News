d41264bdf01e63db2d7ad2b89f3c2b76159df3a3
DarshanN10417 <darshu10.mail@gmail.com>
2025-09-27 12:09:38 +0530

    Update daily_bot.py

--- daily_bot.py
+++ daily_bot.py
@@ -125,6 +125,7 @@ if __name__ == "__main__":
     t = threading.Thread(target=run_scheduler)
     t.daemon = True
     t.start()
+    job()
 
     # Start Flask to keep service alive on Render
     port = int(os.environ.get("PORT", 5000))
